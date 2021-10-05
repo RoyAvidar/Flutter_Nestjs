dd4268d2bdb8d5d497259802524e3db9655ae4d3
Roy Avidar <Roi981av@gmail.com>
2021-10-05 16:16:08 +0300

    graphql_flutter package added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 import '../providers/cart.dart';
 import '../providers/orders.dart';
