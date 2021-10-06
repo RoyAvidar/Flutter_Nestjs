47a6a73787c9724e98b1cf6cace34422a21862ef
Roy Avidar <Roi981av@gmail.com>
2021-10-06 16:32:48 +0300

    a few changes in main.dart with graphql_flutter

--- lib/main.dart
+++ lib/main.dart
@@ -8,23 +8,92 @@ import '../screens/overview_screen.dart';
 import '../widgets/router.dart';
 import 'models/products_provider.dart';
 
+// const productsGraphql = """
+//   query {
+//   products {
+//     productId,
+//     productName,
+//     productPrice,
+//     productDesc,
+//     imageUrl,
+//     category {
+//       categoryName
+//     }
+//   }
+// }
+// """;
+
 void main() {
+  // final HttpLink httpLink = HttpLink("http://localhost:8000/graphql");
+
+  // ValueNotifier<GraphQLClient> client = ValueNotifier(
+  //   GraphQLClient(
+  //     link: httpLink,
+  //     cache: GraphQLCache(store: InMemoryStore()),
+  //   ),
+  // );
+
+  // var app = GraphQLProvider(client: client, child: MyApp());
+
   runApp(MyApp());
 }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter demo',
+//       theme: ThemeData(accentColor: Colors.blue),
+//       home: MyHomePage(),
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({
+//     Key? key,
+//   }) : super(key: key);
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Flutter demo'),
+//       ),
+//       body: Query(
+//         options: QueryOptions(
+//           document: gql(productsGraphql),
+//         ),
+//         builder: (QueryResult result, {fetchMore, reFetch}) {
+//           if (result.hasException) {
+//             return Text(result.exception.toString());
+//           }
+
+//           if (result.isLoading) {
+//             return Center(
+//               child: CircularProgressIndicator(),
+//             );
+//           }
+
+//           final productList = result.data?['products'];
+//           print(productList);
+
+//           return Text("something");
+//         },
+//       ),
+//     );
+//   }
+// }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    //connection to the server.
-    // final HttpLink link = HttpLink(uri: "http://10.0.2.2:4000/");
-
-    // ValueNotifier<GraphQLClient> client =
-    //     ValueNotifier(GraphQLClient(cache: InMemoryCache(), link: link));
-    //return GraphQLProvider(
-    // client: client,
-    //  home: ...,
-    // )
-
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
