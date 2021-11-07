06a8105f6a0014d3deb3f36ddbe15d3d8242545e
Roy Avidar <Roi981av@gmail.com>
2021-11-07 17:01:55 +0200

    some changes

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,18 +1,8 @@
-// import 'dart:convert';
-// import 'dart:async';
-
 import 'package:flutter/widgets.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-// import 'package:http/http.dart' as http;
-// import 'package:shared_preferences/shared_preferences.dart';
-// import '../models/http_exeption.dart';
 
 class AuthProvider with ChangeNotifier {
-  // String _token;
-  // DateTime _expiryDate;
-  // String _userId;
-
-  // AuthProvider(this._token, this._expiryDate);
+  var expireDate;
 
   Future<bool> get isAuth async {
     final prefs = await SharedPreferences.getInstance();
@@ -28,16 +18,4 @@ class AuthProvider with ChangeNotifier {
     // }
     // return null;
   }
-
-//   Future<void> _authenticate(
-//       String email, String password, String urlSegment) async {}
-
-//   Future<void> singup(String userName, String password) async {
-
-//   }
-
-//   Future<void> login(String userName, String password) async {
-
-//   }
-
 }
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_main/config/gql_client.dart';
 import 'package:flutter_main/screens/auth_screen.dart';
 import 'package:flutter_main/screens/overview_screen.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 // import 'package:flutter_main/config/gql_client.dart';
 // import 'package:graphql_flutter/graphql_flutter.dart';
+
+const getExpireDateGraphql = """
+  query {
+    getExpireDate(token: \$token) {
+      getExpireDate
+    }
+  }
+""";
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -14,29 +24,42 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   var token;
+  var expireDate;
+
+  Future<int> _getExpireDate(String token) async {
+    final token = await this.token;
+    QueryOptions queryOptions =
+        QueryOptions(document: gql(getExpireDateGraphql), variables: {
+      'token': token,
+    });
+    QueryResult result = await GraphQLConfig.client.query(queryOptions);
+    if (result.hasException) {
+      print(result.exception);
+    }
+    final prefs = await SharedPreferences.getInstance();
+    final expireDate = result.data?['getExpireDate'];
+    prefs.setInt('expireDate', expireDate);
+    print(expireDate);
+    return expireDate;
+  }
 
   Future<String> _getToken() async {
     final prefs = await SharedPreferences.getInstance();
     final _token = prefs.getString('token');
-    final expiresIn = prefs.getString('expiresIn');
     if (_token!.isEmpty) {
       return token;
-    }
-    // if (expiresIn.isAfter(token.expiryDate)) {
-    //   return false;
-    // }
-    else {
+    } else {
       setState(() {
         token = "Bearer $_token";
       });
+      // print(token);
       return token;
     }
   }
 
   _navHome() async {
     await Future.delayed(Duration(milliseconds: 1500), () {});
-    print(token);
-    if (token == null) {
+    if (expireDate != null) {
       Navigator.pushReplacement(
           context,
           MaterialPageRoute(
@@ -56,6 +79,7 @@ class _SplashScreenState extends State<SplashScreen> {
     // TODO: implement initState
     super.initState();
     _getToken();
+    _getExpireDate(token);
     _navHome();
   }
 
