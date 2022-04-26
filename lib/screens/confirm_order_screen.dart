9c66c8f7d7601b20e1274a767d0380945ee829c7
Roy Avidar <Roi981av@gmail.com>
2022-04-26 15:40:21 +0300

    small changes

--- lib/screens/confirm_order_screen.dart
+++ lib/screens/confirm_order_screen.dart
@@ -69,75 +69,147 @@ class _ConfirmOrderScreenState extends State<ConfirmOrderScreen> {
             "Thank you for your purchase.",
             style: Theme.of(context).textTheme.bodyText1,
           ),
+          Text(
+            "Your order will reach you soon!",
+            style: Theme.of(context).textTheme.bodyText1,
+          ),
           SizedBox(height: 50),
           order == null
               ? Text('Error: no order was found.')
-              : Text(order!.id.toString()),
-          // Expanded(
-          //   child: ListView.builder(
-          //     itemCount: cart!.products!.length,
-          //     itemBuilder: (ctx, i) => Card(
-          //       margin: EdgeInsets.symmetric(
-          //         horizontal: 15,
-          //         vertical: 4,
-          //       ),
-          //       child: Padding(
-          //         padding: EdgeInsets.all(8),
-          //         child: ListTile(
-          //           leading: CircleAvatar(
-          //             child: FittedBox(
-          //               child: Container(
-          //                 width: 300,
-          //                 height: 300,
-          //                 decoration: BoxDecoration(
-          //                   border: Border.all(
-          //                     width: 3,
-          //                     color: Theme.of(context).scaffoldBackgroundColor,
-          //                   ),
-          //                   boxShadow: [
-          //                     BoxShadow(
-          //                       spreadRadius: 1,
-          //                       blurRadius: 5,
-          //                       color: Colors.black.withOpacity(0.5),
-          //                       offset: Offset(0, 15),
-          //                     )
-          //                   ],
-          //                   shape: BoxShape.circle,
-          //                   image: DecorationImage(
-          //                     fit: BoxFit.cover,
-          //                     image: NetworkImage(
-          //                       "http://10.0.2.2:8000/" +
-          //                           cart!.products![i].imageUrl.toString(),
-          //                     ),
-          //                   ),
-          //                 ),
-          //               ),
-          //             ),
-          //           ),
-          //           title: Text('${cart!.products![i].title}'),
-          //           subtitle: Text(
-          //             'Price: \$${(cart!.products![i].price!)}',
-          //           ),
-          //           trailing: Text('x ${cart!.products![i].quantity}'),
-          //         ),
-          //       ),
-          //     ),
-          //   ),
-          // ),
+              : Expanded(
+                  child: ListView.builder(
+                    itemCount: order!.products!.length,
+                    itemBuilder: (ctx, i) => Padding(
+                      padding: EdgeInsets.all(10),
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          child: FittedBox(
+                            child: Container(
+                              width: 300,
+                              height: 300,
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  width: 3,
+                                  color:
+                                      Theme.of(context).scaffoldBackgroundColor,
+                                ),
+                                boxShadow: [
+                                  BoxShadow(
+                                    spreadRadius: 1,
+                                    blurRadius: 5,
+                                    color: Colors.black.withOpacity(0.5),
+                                    offset: Offset(0, 15),
+                                  ),
+                                ],
+                                shape: BoxShape.circle,
+                                image: DecorationImage(
+                                  fit: BoxFit.cover,
+                                  image: NetworkImage(
+                                    "http://10.0.2.2:8000/" +
+                                        order!.products![i].imageUrl.toString(),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        title: Text(order!.products![i].title!),
+                        subtitle: Text(order!.products![i].price!.toString()),
+                        trailing: Text('x ${order!.products![i].quantity}'),
+                      ),
+                    ),
+                  ),
+                ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text(
+                "Total Price: ",
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
+              order == null
+                  ? Text("data")
+                  : Text(
+                      "\$" + order!.totalAmount.toString(),
+                      style: TextStyle(color: Theme.of(context).primaryColor),
+                    ),
+            ],
+          ),
+          Divider(height: 5),
           //show address and payment method.
           ElevatedButton(
-            onPressed: () {},
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: Center(child: Text("Order  #" + order!.id!)),
+                    content: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Address: ",
+                          style: Theme.of(context).textTheme.bodyText1,
+                        ),
+                        Text(
+                          order!.address!,
+                          style: Theme.of(context).textTheme.bodyText2,
+                        ),
+                        Divider(
+                          height: 10,
+                          color: Colors.grey,
+                        ),
+                        Text(
+                          "Created at: ",
+                          style: Theme.of(context).textTheme.bodyText1,
+                        ),
+                        Text(
+                          order!.dateTime.toString(),
+                          style: Theme.of(context).textTheme.bodyText2,
+                        ),
+                        Divider(
+                          height: 10,
+                          color: Colors.grey,
+                        ),
+                        Text(
+                          "Paymet Method: ",
+                          style: Theme.of(context).textTheme.bodyText1,
+                        ),
+                      ],
+                    ),
+                    actions: [
+                      Center(
+                        child: TextButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                          child: Text(
+                            "cancel",
+                            style: TextStyle(
+                                color: Theme.of(context).primaryColor),
+                          ),
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
             style: ElevatedButton.styleFrom(
-              primary: Theme.of(context).primaryColor,
+              primary: Colors.black,
             ),
             child: Text('Order Details'),
           ),
+          Text("Order #" + order!.id!),
           SizedBox(height: 110),
           Container(
-            child: TextButton(
+            child: ElevatedButton(
               child: Text(
                 "Back to main screen",
-                style: TextStyle(color: Colors.grey[600]),
+              ),
+              style: ElevatedButton.styleFrom(
+                primary: Theme.of(context).primaryColor,
               ),
               onPressed: () {
                 Navigator.of(context)
